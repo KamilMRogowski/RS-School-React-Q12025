21eb5f0e0e4dddb20bb9bc062bbae7babeb7ecde
KamilMRogowski <kamil.rogowski94@gmail.com>
2025-01-31 14:59:17 +0100

    feat: update fetch function in homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './views/homePage';
+import HomePage from './views/HomePage';
 
 export default class App extends React.Component {
   render() {
--- src/views/homePage.tsx
+++ src/views/homePage.tsx
@@ -1,37 +1,74 @@
 import React from 'react';
-import SearchBox from '../components/searchBox';
-import ErrorBoundary from '../components/errorBoundary';
-import PokemonApiResponse from '../interfaces/pokemonApiResponse';
+import SearchBox from '../components/SearchBox';
+import Results from '../components/Results';
+import ErrorBoundary from '../components/ErrorBoundary';
+import PokemonApiResponse from '../utils/interfaces/pokemonApiResponse';
 
-export default class HomePage extends React.Component {
+export default class HomePage extends React.Component<
+  object,
+  { results: PokemonApiResponse[]; isLoading: boolean; errorMessage: string }
+> {
   state = {
-    query: '',
     results: [],
+    isLoading: false,
+    errorMessage: '',
   };
 
   // Update the query state when recieved from the SearchBox component
   onSearch = (query: string) => {
-    this.setState({ query });
-    this.fetchDataFromAPI(query);
+    // Use .catch to handle errors from the API call
+    this.fetchDataFromAPI(query).catch((error: unknown) => {
+      console.error('API Call failed', error);
+    });
   };
 
   // Fetch data from the pokeapi.co API and update the results state
-  fetchDataFromAPI = (query: string) => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-      .then((response) => response.json())
-      .then((data: PokemonApiResponse) => {
-        console.log(data);
-        this.setState({ results: data });
-      })
-      .catch((error: unknown) => {
-        console.error(error);
+  fetchDataFromAPI = async (query: string) => {
+    // Set state to loading and reset error message
+    this.setState({ isLoading: true, errorMessage: '' });
+
+    const pokemonLimit = 10;
+    const url = `https://pokeapi.co/api/v2/pokemon/${query}?limit=${String(pokemonLimit)}`;
+
+    try {
+      const response = await fetch(url);
+
+      // If pokemon not found, throw an error
+      if (!response.ok) {
+        throw new Error('Pokemon not found, please try again!');
+      }
+
+      const data = (await response.json()) as PokemonApiResponse[];
+      this.setState({ results: data, isLoading: false });
+    } catch (error: unknown) {
+      console.error(error);
+      // Checks if error is an instance of Error, if not, sets a generic error message
+      if (error instanceof Error) {
+        this.setState({
+          errorMessage: error.message,
+        });
+      } else {
+        this.setState({
+          errorMessage: 'An unknown error occurred',
+        });
+      }
+      // Reset results and loading state
+      this.setState({
+        results: [],
+        isLoading: false,
       });
+    }
   };
 
   render() {
     return (
       <ErrorBoundary>
         <SearchBox onSearch={this.onSearch}></SearchBox>
+        <Results
+          results={this.state.results}
+          isLoading={this.state.isLoading}
+          errorMessage={this.state.errorMessage}
+        ></Results>
       </ErrorBoundary>
     );
   }
