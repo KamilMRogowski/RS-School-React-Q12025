3285abbaaad59ea267850898a01a97ae35e4c9f1
KamilMRogowski <kamil.rogowski94@gmail.com>
2025-02-05 10:57:21 +0100

    feat: transform fetchAPI function into custom hook

--- src/services/fetchPokemonFromAPI.ts
+++ src/services/fetchPokemonFromAPI.ts
@@ -1,22 +1,35 @@
-import { PokemonApiResponse } from '../utils/interfaces/pokemonApiResponse';
+import { useEffect, useState } from 'react';
+import {
+  PokemonApiResponse,
+  pokemonListResponse,
+} from '../utils/interfaces/pokemonApiResponse';
 
-export default async function fetchPokemonFromAPI(
-  query: string
-): Promise<PokemonApiResponse | string> {
-  const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error('Pokemon not found, please try again!');
-    }
-
-    const data = (await response.json()) as PokemonApiResponse;
-    return data;
-  } catch (error: unknown) {
-    console.error(error);
-    if (error instanceof Error) {
-      return error.message;
-    } else return 'An unknown error occurred';
-  }
+export default function useFetchPokemonFromAPI(url: string, query: string) {
+  const [data, setData] = useState<PokemonApiResponse>(pokemonListResponse);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>('');
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError('');
+      try {
+        const response = await fetch(`${url}${query}`);
+        if (!response.ok) {
+          throw new Error('Pokemon not found, please try again!');
+        }
+        const data = (await response.json()) as PokemonApiResponse;
+        setData(data);
+        localStorage.setItem('lastSearch', query);
+      } catch (error: unknown) {
+        console.error(error);
+        setError(
+          error instanceof Error ? error.message : 'An unknown error occurred'
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+    void fetchData();
+  }, [url, query]);
+  return { data, loading, error };
 }
