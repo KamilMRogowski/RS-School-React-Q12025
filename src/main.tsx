98e219375b8b8aaa75948c740909eaebebfd0169
KamilMRogowski <kamil.rogowski94@gmail.com>
2025-02-09 20:03:55 +0100

    fix: change to hashrouter to work correct with gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter basename="/RS-School-React-Q12025">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
