67aff507f629f93ce3807eefced90531ea875781
KamilMRogowski <kamil.rogowski94@gmail.com>
2025-03-06 17:56:59 +0100

    fix: remove basepath from config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: './dist', // Changes the build output directory to `./dist/`.
-  basePath: '/RS-School-React-Q12025', // Sets the base path for the application.
+  // basePath: '/RS-School-React-Q12025', // Basepath for gh-pages deploy
   sassOptions: {
     includePaths: ['./src/styles'],
   },
